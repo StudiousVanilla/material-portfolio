40c17e34da4bceb7c9dc8e78d3950ee78b2d83b0
Oisín_windows <byrne.ois@gmail.com>
2021-03-19 15:38:44 +0000

    2nd commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import ResponsiveDrawer from './components/ResponsiveDrawer'
+import {createMuiTheme, ThemeProvider} from '@material-ui/core'
+import { teal } from '@material-ui/core/colors';
+
+const theme = createMuiTheme({
+  palette: {
+    secondary: teal,
+    primary: teal
+  }
+})
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <div>
+        <ResponsiveDrawer/>
+      </div>
+    </ThemeProvider>
   );
 }
 
